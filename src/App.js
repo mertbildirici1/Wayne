4194c0d16c48916323dab077520024be6c07d8f0
Halit Bildirici <halit.bildirici@duke.edu>
2023-08-18 13:51:41 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code> and save to reload.
         </p>
         <p>WELCOME TO THE WAYNE WEBSITE</p>
         <a
